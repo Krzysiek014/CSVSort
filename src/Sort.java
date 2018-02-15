82a568d76544046ee9490abe6342a3de0faf50f7
Krzysiek014 <krzysiek014@o2.pl>
2018-02-15 14:55:59 +0100

    Added ui to choose by which column sort csv file

--- src/Sort.java
+++ src/Sort.java
@@ -9,10 +9,11 @@ public class Sort {
 
     private static File file;
     private static List<String[]> list;
-    private static String fileUri = "";
-
+    private static String path = "";
+    private static int column;
+    private static Scanner cmdScan;
     public static void main(String[] args) throws FileNotFoundException {
-
+        cmdScan = new Scanner(System.in);
         //Getting file path
         getFile();
 
@@ -24,35 +25,52 @@ public class Sort {
         }
         scan.close();
 
-        sort(0);
+        chooseColumn();
+
+        cmdScan.close();
+
+        sort(column);
 
         for(String[] s: list)
-        System.out.println(s[0]);
+        System.out.println(s[0] + "          "+ s[1]);
     }
 
     private static void sort(int i){
-
+        String[] header = list.get(0);
+        list.remove(0);
         Collections.sort(list, new Comparator<String[]>() {
             @Override
             public int compare(String[] o1, String[] o2) {
                 return o1[i].compareTo(o2[i]);
             }
         });
-
+        list.add(0,header);
     }
 
-    public static void getFile(){
-        Scanner cmdScan = new Scanner(System.in);
+    private static void getFile(){
         do {
             System.out.println("Enter path to your .csv file...");
-            fileUri = cmdScan.nextLine();
-        } while (!fileUri.matches("(.+[\\.]csv)"));
-        file = new File(fileUri);
-        if(file.exists()) {
-            cmdScan.close();
-        }else{
-            System.out.println("File "+fileUri+" does not exist");
+            path = cmdScan.nextLine();
+        } while (!path.matches("(.+[\\.]csv)"));
+        file = new File(path);
+        if(!file.exists()) {
+            System.out.println("File "+path+" does not exist");
             getFile();
         }
     }
+
+    private static void chooseColumn(){
+        System.out.println("Choose column which should be sorted:");
+        for(int i=0;i<list.get(0).length;i++)
+            System.out.println(i+")  " + list.get(0)[i]);
+        System.out.println("Enter number corresponding to a column");
+
+        try {
+            column = cmdScan.nextInt();
+        }catch(InputMismatchException e){
+            chooseColumn();
+        }
+
+    }
+
 }
