13ad60b651531394984916f0e5ed8e1cab1a1ead
Krzysiek014 <krzysiek014@o2.pl>
2018-02-15 14:38:37 +0100

    Added verification for file existence and basic sorting

--- src/Sort.java
+++ src/Sort.java
@@ -1,8 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Krzysiek014 on 14.02.2018.
@@ -10,18 +8,51 @@ import java.util.Scanner;
 public class Sort {
 
     private static File file;
+    private static List<String[]> list;
+    private static String fileUri = "";
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner cmdScan = new Scanner(System.in);
+        //Getting file path
+        getFile();
 
-        System.out.println("Enter URI to your .csv file...");
-        file = new File(cmdScan.nextLine());
-
+        // Scan .csv for entries
         Scanner scan = new Scanner(file);
-        List<String[]> list = new LinkedList<>();
+        list = new LinkedList<>();
         while(scan.hasNextLine()) {
             list.add(scan.nextLine().split(","));
+        }
+        scan.close();
+
+        sort(0);
+
+        for(String[] s: list)
+        System.out.println(s[0]);
+    }
+
+    private static void sort(int i){
+
+        Collections.sort(list, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return o1[i].compareTo(o2[i]);
+            }
+        });
+
+    }
+
+    public static void getFile(){
+        Scanner cmdScan = new Scanner(System.in);
+        do {
+            System.out.println("Enter path to your .csv file...");
+            fileUri = cmdScan.nextLine();
+        } while (!fileUri.matches("(.+[\\.]csv)"));
+        file = new File(fileUri);
+        if(file.exists()) {
+            cmdScan.close();
+        }else{
+            System.out.println("File "+fileUri+" does not exist");
+            getFile();
         }
     }
 }
